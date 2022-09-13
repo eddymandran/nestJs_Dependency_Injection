27ca68d7eb1f0f0f680acd36d83837c6b5ff4f50
eddymandran <eddy.mandran@gmail.com>
2022-09-13 14:50:36 +0200

    setting up Dependency Injection between power and disk

--- src/cpu/cpu.service.ts
+++ src/cpu/cpu.service.ts
@@ -5,4 +5,11 @@ import { PowerService } from "../power/power.service";
 export class CpuService {
   constructor(private powerService: PowerService) {
   }
+
+  compute(a: number, b: number) {
+    console.log("Drawing 10 watts of power from Power Service");
+    this.powerService.supplyPower(10);
+    return a + b;
+  }
 }
+
